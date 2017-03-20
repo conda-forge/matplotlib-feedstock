3b58e6758f804c39308b5cf985b8cf8ec7107f48
ocefpaf <ocefpaf@gmail.com>
2017-03-20 14:25:11 -0300

    remove unnecessary workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
-
-
 if [ $(uname) == Linux ]; then
     pushd $PREFIX/lib
     ln -s libtcl8.5.so libtcl.so
