fe7fda33bf3129ffb442e382bf3ed1d04d2880e7
Ryan May <rmay@ucar.edu>
2017-04-24 17:30:49 -0600

    Build against local freetype

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,8 @@ ECHO [packages] >> setup.cfg
 ECHO tests = False >> setup.cfg
 ECHO sample_data = False >> setup.cfg
 ECHO toolkits_tests = False >> setup.cfg
+ECHO [test] >> setup.cfg
+ECHO local_freetype = True >> setup.cfg
 
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
