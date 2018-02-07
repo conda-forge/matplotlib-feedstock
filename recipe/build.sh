fc15c2fc9e6ff12b2f6481fdd30ed47a88395854
Ryan May <rmay@ucar.edu>
2018-02-06 17:06:10 -0700

    Create build directory

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p build
 curl https://download.savannah.gnu.org/releases/freetype/freetype-2.6.1.tar.gz -o build/freetype-2.6.1.tar.gz
 if [ $(uname) == Linux ]; then
     pushd $PREFIX/lib
