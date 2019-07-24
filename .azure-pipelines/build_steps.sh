e94f0934b8d468b03ea96c62949766b928e64437
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-24 13:51:25 -0300

    MNT: Re-rendered with conda-build 3.18.9, conda-smithy 3.4.1, and conda-forge-pinning 2019.07.23

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -20,6 +20,9 @@ conda-build:
 CONDARC
 
 conda install --yes --quiet conda-forge-ci-setup=2 conda-build -c conda-forge
+# workaround until the pinning issued is fixed in conda-build
+conda install conda-build=3.17.8 --yes
+conda clean --all --yes
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
