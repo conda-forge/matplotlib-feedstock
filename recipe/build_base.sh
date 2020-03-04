816cbe18bc23de55064dbc504d192b53b0c14511
Filipe Fernandes <ocefpaf@gmail.com>
2020-03-04 13:36:49 -0300

    remove links

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ $(uname) == Linux ]; then
-    pushd $PREFIX/lib
-    ln -s libtcl8.6.so libtcl.so
-    ln -s libtk8.6.so libtk.so
-    popd
-fi
-
 cat <<EOF > setup.cfg
 [directories]
 basedirlist = $PREFIX
@@ -20,7 +13,6 @@ EOF
 
 
 cat setup.cfg
-sed -i.bak "s|/usr/local|$PREFIX|" setupext.py
+sed -i.bak "s|/usr/local|${PREFIX}|" setupext.py
 
-
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
+${PYTHON} -m pip install . --no-deps -vv
