3820bd4261136a810e05bf1c154cdcccfa804437
Isuru Fernando <isuruf@gmail.com>
2020-04-14 02:03:09 -0500

    set MPLSTATICBUILD

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -15,6 +15,7 @@ ECHO local_freetype = True >> setup.cfg
 
 rem matplotlib will link to png statically if it exists. Delete to make sure a shared version is picked up
 del %LIBRARY_LIB%\libpng16_static.lib
+set "MPLSTATICBUILD="
 
 %PYTHON% -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
