5281ae4a76789bb32e2c3f5b90b87de49f79e19e
Ryan May <rmay@ucar.edu>
2020-07-18 15:54:34 -0600

    Remove unneeded handling of static libraries on windows
    Matplotlib no longer depends on these.

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -9,11 +9,5 @@ ECHO tests = False >> setup.cfg
 ECHO sample_data = False >> setup.cfg
 ECHO toolkits_tests = False >> setup.cfg
 
-rem matplotlib will link to png, zlib statically if it exists. Replace the static library with shared
-del %LIBRARY_LIB%\libpng16_static.lib
-copy %LIBRARY_LIB%\libpng16.lib %LIBRARY_LIB%\libpng16_static.lib
-del %LIBRARY_LIB%\zlibstatic.lib
-copy %LIBRARY_LIB%\z.lib %LIBRARY_LIB%\zlibstatic.lib
-
 %PYTHON% -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
