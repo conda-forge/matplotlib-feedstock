6988a6a1593208432644cad94b941924d4c15987
jakirkham <jakirkham@gmail.com>
2024-06-26 16:46:19 -0700

    Include additional Meson build steps

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,7 +1,14 @@
+@echo on
+
 set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
 mkdir builddir
+if errorlevel 1 exit 1
 %PYTHON% -m mesonbuild.mesonmain setup %MESON_ARGS% builddir
+if errorlevel 1 exit 1
+%PYTHON% -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+if errorlevel 1 exit 1
+for %x in (dir *.whl) do %PYTHON% -m pip install %x
 if errorlevel 1 exit 1
