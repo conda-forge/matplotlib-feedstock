8c8807577b1ec9784120069921b097eea35b6a36
jakirkham <jakirkham@gmail.com>
2024-06-26 15:59:47 -0700

    Drop now unneeded `conda_mpl_config.cfg`

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -2,15 +2,6 @@ set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
-ECHO [directories] > mplsetup.cfg
-ECHO basedirlist = %LIBRARY_PREFIX% >> mplsetup.cfg
-ECHO [packages] >> mplsetup.cfg
-ECHO tests = False >> mplsetup.cfg
-ECHO sample_data = False >> mplsetup.cfg
-ECHO toolkits_tests = False >> mplsetup.cfg
-
-set MPLSETUPCFG=mplsetup.cfg
-
 mkdir builddir
 %PYTHON% -m mesonbuild.mesonmain setup %MESON_ARGS% builddir
 if errorlevel 1 exit 1
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-cat <<EOF > conda_mpl_config.cfg
-[directories]
-basedirlist = $PREFIX
-
-[packages]
-tests = False
-toolkit_tests = False
-sample_data = False
-
-EOF
-
-cat conda_mpl_config.cfg
-
-export MPLSETUPCFG=conda_mpl_config.cfg
 export AR=$GCC_AR
 
 mkdir builddir
