5c853e823f69dfa9c0e7d760590bee5ede6d59f6
Thomas A Caswell <tcaswell@bnl.gov>
2024-06-26 11:51:35 -0400

    MNT: attempt to fix build

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -9,13 +9,11 @@ tests = False
 toolkit_tests = False
 sample_data = False
 
-[libs]
-system_freetype = True
-
 EOF
 
 cat conda_mpl_config.cfg
 
 export MPLSETUPCFG=conda_mpl_config.cfg
+export AR=$GCC_AR
 
-$PYTHON -m pip install --no-deps --no-build-isolation -vv .
+$PYTHON -m pip install --no-deps --no-build-isolation -vv . --config-settings=setup-args="-Dsystem-freetype=true"
