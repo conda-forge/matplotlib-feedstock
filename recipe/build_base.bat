5bc1a4e613898b09b29335d0d48da07417e4e10a
jakirkham <jakirkham@gmail.com>
2024-06-26 18:27:20 -0700

    Use system Qhull

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -7,6 +7,13 @@ if errorlevel 1 exit 1
 %PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS%
 if errorlevel 1 exit 1
 %PYTHON% -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+%PYTHON% -m build ^
+	 --wheel ^
+	 --no-isolation ^
+	 --skip-dependency-check ^
+	 -Cbuilddir=builddir ^
+	 -Csetup-args="-Dsystem-qhull=true" ^
+
 if errorlevel 1 exit 1
 for %x in (dir *.whl) do (%PYTHON% -m pip install %x)
 if errorlevel 1 exit 1
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -8,5 +8,11 @@ export MESON_ARGS="${MESON_ARGS} --buildtype=release -Dpython_target=${PYTHON}"
 
 mkdir builddir
 $PYTHON -m mesonbuild.mesonmain setup builddir $MESON_ARGS
-$PYTHON -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+$PYTHON -m build \
+	--wheel \
+	--no-isolation \
+	--skip-dependency-check \
+	-Cbuilddir=builddir \
+	-Csetup-args="-Dsystem-qhull=true" \
+
 $PYTHON -m pip install dist/matplotlib*.whl
