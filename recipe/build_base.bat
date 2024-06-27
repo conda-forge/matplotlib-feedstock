9d8b89be891ec299764dc46651620ff408660d1b
jakirkham <jakirkham@gmail.com>
2024-06-27 15:42:22 -0700

    Output Meson logs after `setup`

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -5,9 +5,9 @@ set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg
 mkdir builddir
 if errorlevel 1 exit 1
 %PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS%
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
 %PYTHON% -m build --wheel ^
          --no-isolation --skip-dependency-check -Cbuilddir=builddir -Ccompile-args=-v
-if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
+if errorlevel 1 exit 1
 %PYTHON% -m pip install --find-links dist matplotlib
 if errorlevel 1 exit 1
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -7,8 +7,8 @@ export AR=$GCC_AR
 export MESON_ARGS="${MESON_ARGS} --buildtype=release --prefix=${PREFIX} -Dlibdir=lib --pkg-config-path=${PREFIX}/lib/pkgconfig -Dsystem-freetype=true -Dsystem-qhull=true"
 
 mkdir builddir
-$PYTHON -m mesonbuild.mesonmain setup builddir $MESON_ARGS
-$PYTHON -m build --wheel \
-        --no-isolation --skip-dependency-check -Cbuilddir=builddir \
+$PYTHON -m mesonbuild.mesonmain setup builddir $MESON_ARGS \
         || (cat builddir/meson-logs/meson-log.txt && exit 1)
+$PYTHON -m build --wheel \
+        --no-isolation --skip-dependency-check -Cbuilddir=builddir
 $PYTHON -m pip install --find-links dist matplotlib
