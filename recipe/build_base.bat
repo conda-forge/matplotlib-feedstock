6028fbbe0d4885f738a08c0b0a07fced21bdec04
jakirkham <jakirkham@gmail.com>
2024-06-26 17:33:10 -0700

    Drop Windows env var workarounds

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,9 +1,5 @@
 @echo on
 
-set LIB=%LIBRARY_LIB%;%LIB%
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
 mkdir builddir
 if errorlevel 1 exit 1
 %PYTHON% -m mesonbuild.mesonmain setup %MESON_ARGS% builddir
