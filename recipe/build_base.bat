dabeec4546b850cf268ce3d7b4035f5ed2f23051
jakirkham <jakirkham@gmail.com>
2024-06-27 00:56:40 -0700

    Try `libdir` as `lib` on Windows

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,6 +1,6 @@
 @echo on
 
-set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg-config-path=%LIBRARY_LIB%\pkgconfig -Dlibdir=bin -Dsystem-freetype=true -Dsystem-qhull=true"
+set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg-config-path=%LIBRARY_LIB%\pkgconfig -Dlibdir=lib -Dsystem-freetype=true -Dsystem-qhull=true"
 
 mkdir builddir
 if errorlevel 1 exit 1
