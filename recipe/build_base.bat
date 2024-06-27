ccb75bb189ea45f6253d68f6ed56992d55698a84
jakirkham <jakirkham@gmail.com>
2024-06-27 04:02:17 -0700

    Also on error print Meson logs

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -6,7 +6,8 @@ mkdir builddir
 if errorlevel 1 exit 1
 %PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS%
 if errorlevel 1 exit 1
-%PYTHON% -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir -Ccompile-args=-v
-if errorlevel 1 exit 1
+%PYTHON% -m build --wheel ^
+         --no-isolation --skip-dependency-check -Cbuilddir=builddir -Ccompile-args=-v
+if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
 %PYTHON% -m pip install --find-links dist matplotlib
 if errorlevel 1 exit 1
--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -8,5 +8,7 @@ export MESON_ARGS="${MESON_ARGS} --buildtype=release --prefix=${PREFIX} -Dlibdir
 
 mkdir builddir
 $PYTHON -m mesonbuild.mesonmain setup builddir $MESON_ARGS
-$PYTHON -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+$PYTHON -m build --wheel \
+        --no-isolation --skip-dependency-check -Cbuilddir=builddir \
+        || (cat builddir/meson-logs/meson-log.txt && exit 1)
 $PYTHON -m pip install --find-links dist matplotlib
