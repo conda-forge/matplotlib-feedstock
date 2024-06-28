6355da0f64cacc8f2c928245a94720f11d5bc648
jakirkham <jakirkham@gmail.com>
2024-06-27 18:36:35 -0700

    Tidy `robocopy` call

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -5,9 +5,8 @@ set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg
 if "%CI%" == "azure" (
     :: Hack to try removing problematic Python from Azure CI image
     mkdir C:\empty
-    robocopy /mir /ns /nc /nfl /ndl /np /njh /njs C:\empty C:\hostedtoolcache\windows\Python > nul 2>&1
+    robocopy /mir /ns /nc /nfl /ndl /np /njh /njs C:\empty C:\hostedtoolcache\windows\Python > nul 2>&1 && rmdir /q C:\hostedtoolcache\windows\Python
     rmdir /q C:\empty
-    rmdir /q C:\hostedtoolcache\windows\Python
 )
 
 mkdir builddir
