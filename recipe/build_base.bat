032ac089454c1d10ee6aa191276ce0b19071a2f8
jakirkham <jakirkham@gmail.com>
2024-06-29 01:17:39 -0700

    Simplify Azure Python cleanup

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -6,19 +6,9 @@ if "%CI%" == "azure" (
     :: Hack to try removing problematic Python from Azure CI image
     :: Replace with conda-smithy solution when available
     :: xref: https://github.com/conda-forge/conda-smithy/pull/1966
-    set CLEANUP_DIRS=^
-    C:\hostedtoolcache\windows\Python;^
-    ;
-
     mkdir C:\empty
-    for %%f in (%CLEANUP_DIRS:;= %) do (
-        if not [%%f] == [] (
-            echo Removing %%f
-            dir %%f
-            robocopy /purge /r:0 /w:0 /mt /ns /nc /np /nfl /ndl /njh /njs C:\empty %%f > nul 2>&1
-            rmdir /q %%f
-        )
-    )
+    robocopy /purge /r:0 /w:0 /mt /ns /nc /np /nfl /ndl /njh /njs C:\empty C:\hostedtoolcache\windows\Python > nul 2>&1
+    rmdir /q C:\hostedtoolcache\windows\Python
     rmdir /q C:\empty
 )
 
