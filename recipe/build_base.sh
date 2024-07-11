5e5b4968bf9bbe920fcacb1cb5c5472d41bb9dc9
H. Vetinari <h.vetinari@gmx.com>
2024-07-11 21:52:10 +1100

    do not call meson directly; build through meson-python

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -ex
 
 export AR=$GCC_AR
@@ -8,9 +7,14 @@ export AR=$GCC_AR
 export MESON_ARGS="${MESON_ARGS} --pkg-config-path=${PREFIX}/lib/pkgconfig"
 export MESON_ARGS="${MESON_ARGS} -Dsystem-freetype=true -Dsystem-qhull=true"
 
-mkdir builddir
-$PYTHON -m mesonbuild.mesonmain setup builddir $MESON_ARGS
-cat builddir/meson-logs/meson-log.txt
-$PYTHON -m build --wheel \
-        --no-isolation --skip-dependency-check -Cbuilddir=builddir
+# meson-python already sets up a -Dbuildtype=release argument to meson, so
+# we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
+MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+
+# -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+$PYTHON -m build -w -n -x \
+    -Cbuilddir=builddir \
+    -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
+    || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
 $PYTHON -m pip install --find-links dist matplotlib
