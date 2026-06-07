b657d19e67907ac1e21f5ccbc658117dc115249f
H. Vetinari <h.vetinari@gmx.com>
2026-06-08 10:40:51 +1100

    clean up error handling in build_base.bat

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,11 +1,14 @@
 @echo on
 
 mkdir builddir
-if errorlevel 1 exit 1
+
 %PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS% ^
     -Dsystem-freetype=true -Dsystem-libraqm=true -Dsystem-qhull=true
+if %ERRORLEVEL% NEQ 0 (type builddir\meson-logs\meson-log.txt && exit 1)
+
 %PYTHON% -m build --wheel ^
          --no-isolation --skip-dependency-check -Cbuilddir=builddir -Ccompile-args=-v
-if errorlevel 1 exit 1
+if %ERRORLEVEL% NEQ 0 exit 1
+
 %PYTHON% -m pip install --find-links dist matplotlib
-if errorlevel 1 exit 1
+if %ERRORLEVEL% NEQ 0 exit 1
