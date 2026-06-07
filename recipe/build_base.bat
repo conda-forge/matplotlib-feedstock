016fe3e11c2bddcbf62b585ebd3cc41ee3e0036c
H. Vetinari <h.vetinari@gmx.com>
2026-06-08 10:23:18 +1100

    delete old work-around

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -2,16 +2,6 @@
 
 set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg-config-path=%LIBRARY_LIB%\pkgconfig -Dlibdir=lib -Dsystem-freetype=true -Dsystem-libraqm=true -Dsystem-qhull=true"
 
-if "%CI%" == "azure" (
-    :: Hack to try removing problematic Python from Azure CI image
-    :: Replace with conda-smithy solution when available
-    :: xref: https://github.com/conda-forge/conda-smithy/pull/1966
-    mkdir C:\empty
-    robocopy /purge /r:0 /w:0 /mt /ns /nc /np /nfl /ndl /njh /njs C:\empty C:\hostedtoolcache\windows\Python > nul 2>&1
-    rmdir /q C:\hostedtoolcache\windows\Python
-    rmdir /q C:\empty
-)
-
 mkdir builddir
 if errorlevel 1 exit 1
 %PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS%
