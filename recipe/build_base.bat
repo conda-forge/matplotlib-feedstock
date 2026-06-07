5190973c34e0c766b9e99de0cf0b420467144d0f
H. Vetinari <h.vetinari@gmx.com>
2026-06-08 10:40:05 +1100

    remove redundant MESON_ARGS already handled by compiler activation

--- recipe/build_base.bat
+++ recipe/build_base.bat
@@ -1,11 +1,9 @@
 @echo on
 
-set "MESON_ARGS=%MESON_ARGS% --buildtype=release --prefix=%LIBRARY_PREFIX% --pkg-config-path=%LIBRARY_LIB%\pkgconfig -Dlibdir=lib -Dsystem-freetype=true -Dsystem-libraqm=true -Dsystem-qhull=true"
-
 mkdir builddir
 if errorlevel 1 exit 1
-%PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS%
-type builddir\meson-logs\meson-log.txt
+%PYTHON% -m mesonbuild.mesonmain setup builddir %MESON_ARGS% ^
+    -Dsystem-freetype=true -Dsystem-libraqm=true -Dsystem-qhull=true
 %PYTHON% -m build --wheel ^
          --no-isolation --skip-dependency-check -Cbuilddir=builddir -Ccompile-args=-v
 if errorlevel 1 exit 1
